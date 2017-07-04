7bde47d924d0f23e5af725c6a57841e46762e7f7
Jake Pulford <jpulford01@qub.ac.uk>
2017-07-04 08:54:31 +0100

    Please work

---
+++ JavaChallenges/JavaChallenge1/src/main/java/java_challenge_1/Runner.java
@@ -0,0 +1,27 @@
+package java_challenge_1;
+
+/**
+ * Created by jpulford on 20/06/17.
+ * Run to execute JavaChallenge1 classes
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // txt file to read in
+        String fileNameIn = "src/main/resources/num.txt";
+
+        PartASumValues partA = new PartASumValues();
+        PartBAsciiAdder partB = new PartBAsciiAdder();
+        PartCOrder partC = new PartCOrder();
+        PartDRemoveBlanks partD = new PartDRemoveBlanks();
+
+        partA.sumTotal(fileNameIn);
+        partB.getAsciiValues(fileNameIn, "num2.txt" );
+        partC.orderNumbersInFile(fileNameIn, "num3.txt");
+        partD.removeBlanks(fileNameIn, "num4.txt");
+
+
+
+    }
+}
---
+++ TestProject/src/TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jpulford on 04/07/17.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
